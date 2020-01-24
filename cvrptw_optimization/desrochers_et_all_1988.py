b0e072ae657466768e6111eeafc614f2b517bd59
ecimren <ecimren@starbucks.com>
2020-01-24 00:42:13 -0800

    Added desrochers inputs

--- cvrptw_optimization/desrochers_et_all_1988.py
+++ cvrptw_optimization/desrochers_et_all_1988.py
@@ -5,7 +5,29 @@ CVRPTW formulation by Desrochers et al 1988
     In: Golden, B.L., Assad, A.A. (Eds.), Vehicle Routing: Methods and Studies. North-Holland, Amsterdam, pp. 65–84.
 '''
 
+import pandas as pd
+from cvrptw_optimization.src import data
+from cvrptw_optimization.src import desrochers_et_all_1988_inputs as di
 
-def run_desrochers_et_all_1988(solver='ortools'):
+
+def run_desrochers_et_all_1988(depot,
+                               locations,
+                               transportation_matrix,
+                               solver='ortools'):
+
+    locations_name_list = list(locations['LOCATION_NAME'].unique())
+    depot_name_list = list(depot['LOCATION_NAME'].unique())
+
+    # filter transportation matrix
+    transportation_matrix = di.filter_transportation_matrix(transportation_matrix, locations_name_list, depot_name_list)
+
+    # validate transportation matrix
+    di.validate_transportation_matrix(transportation_matrix, locations_name_list, depot_name_list)
+
+    # convert transportation matrix
+    transportation_matrix = di.prepare_transportation_matrix(transportation_matrix, depot_name_list)
+
+    # prepare model inputs
+
 
     return None
--- cvrptw_optimization/src/desrochers_et_all_1988_inputs.py
+++ cvrptw_optimization/src/desrochers_et_all_1988_inputs.py
@@ -1,7 +1,44 @@
 '''
+Input data sets
 CVRPTW formulation by Desrochers et al 1988
     Desrochers, M., Lenstra, J.K., Savelsbergh, M.W.P., Soumis, F. (1988).
     Vehicle routing with time windows: Optimization and approximation.
     In: Golden, B.L., Assad, A.A. (Eds.), Vehicle Routing: Methods and Studies. North-Holland, Amsterdam, pp. 65–84.
 '''
+
+
+class ModelInputs:
+
+    def __init__(self, transportation_matrix, locations, depot, maximum_trucks):
+
+        self.transportation_matrix = transportation_matrix
+        self.locations = locations
+        self.depot = depot
+        self.depot_name = depot['LOCATION_NAME'].iloc[0]
+        self.depot_leave = '{}_START'.format(self.depot_name)
+        self.depot_enter = '{}_END'.format(self.depot_name)
+        self.maximum_trucks = maximum_trucks
+
+        self.K = None
+        self.V = None
+        self.N = None
+
+        self.t = None
+
+    def create_set_of_vehicles(self):
+        self.K = list(range(0, self.maximum_trucks))
+
+    def create_set_of_vertices(self):
+        self.V = [self.depot_leave] + list(self.locations['LOCATION_NAME'].unique()) + [self.depot_enter]
+
+    def create_set_of_nodes(self):
+        self.N = list(self.locations['LOCATION_NAME'].unique())
+
+    def create_transit_minutes(self):
+        self.t = {}
+        for row in self.transportation_matrix.itertuples():
+            if (row.FROM_LOCATION_NAME in self.V) & (row.TO_LOCATION_NAME in self.V):
+                self.t[row.FROM_LOCATION_NAME, row.TO_LOCATION_NAME] = row.DRIVE_MINUTES
+
+
 
