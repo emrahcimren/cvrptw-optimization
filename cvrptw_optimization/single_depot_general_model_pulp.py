962383e5aac3763d49d7147f012ba2a124becd94
emrahcimren <ecimren@starbucks.com>
2020-04-04 23:28:57 -0700

    Updated solver inputs

--- cvrptw_optimization/single_depot_general_model_pulp.py
+++ cvrptw_optimization/single_depot_general_model_pulp.py
@@ -5,16 +5,26 @@ from cvrptw_optimization.src import single_depot_general_model_pulp_formulation 
 def run_single_depot_general_model(depots,
                                    customers,
                                    transportation_matrix,
-                                   vehicles):
+                                   vehicles,
+                                   bigm=100000000,
+                                   mip_gap=0.001,
+                                   solver_time_limit_minutes=10,
+                                   enable_solution_messaging=1,
+                                   solver_type='PULP_CBC_CMD'
+                                   ):
     '''
     Run single depot general model
     :param depots:
     :param customers:
     :param transportation_matrix:
     :param vehicles:
+    :param bigm:
+    :param mip_gap:
+    :param solver_time_limit_minutes:
+    :param enable_solution_messaging:
+    :param solver_type:
     :return:
     '''
-
     print('Running Single Depot General Model')
 
     print('Getting model inputs')
@@ -31,10 +41,13 @@ def run_single_depot_general_model(depots,
                                          depots['LOCATION_NAME'].iloc[0]
                                          )
     print('Formulating the problem')
-    model.formulate_problem()
+    model.formulate_problem(bigm)
 
     print('Solving the model')
-    model.solve_model()
+    model.solve_model(mip_gap,
+                      solver_time_limit_minutes,
+                      enable_solution_messaging,
+                      solver_type)
 
     print('Getting model results')
     model.get_model_solution()
